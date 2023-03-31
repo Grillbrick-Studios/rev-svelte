5c7f3f24106f1e541daf50100d0767db0600f58f
Jim Hessin <jhessin@gmail.com>
2023-03-31 15:42:59 -0600

    Create InfoBox start making settings

--- src/routes/preferences/+page.svelte
+++ src/routes/preferences/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import moreInfo from '$lib/assets/moreinfo.png';
+	import { BibleTextMode, bibleTextMode } from '$lib/settings';
+	import InfoBox from './InfoBox.svelte';
 </script>
 
 <p>
@@ -16,3 +18,75 @@
 	preferences, the website will remember whatever settings you choose, even if
 	you close your web browser and come back later.
 </p>
+
+<table style="width: 100%;">
+	<InfoBox>
+		<div slot="name">Bible Text Mode</div>
+		<table slot="options" cellspacing="0" cellpadding="0" border="0">
+			<tr>
+				<td>
+					<input
+						type="radio"
+						name={BibleTextMode.VerseBreak}
+						checked={$bibleTextMode === BibleTextMode.VerseBreak}
+						on:click={() => ($bibleTextMode = BibleTextMode.VerseBreak)}
+					/>
+				</td>
+				<td>Verse Break</td>
+			</tr>
+			<tr>
+				<td>
+					<input
+						type="radio"
+						name={BibleTextMode.Paragraph}
+						checked={$bibleTextMode === BibleTextMode.Paragraph}
+						on:click={() => ($bibleTextMode = BibleTextMode.Paragraph)}
+					/>
+				</td>
+				<td>Paragraph</td>
+			</tr>
+			<tr>
+				<td>
+					<input
+						type="radio"
+						name={BibleTextMode.Reading}
+						checked={$bibleTextMode === BibleTextMode.Reading}
+						on:click={() => ($bibleTextMode = BibleTextMode.Reading)}
+					/>
+				</td>
+				<td>Reading</td>
+			</tr>
+		</table>
+		<div
+			id="prefhelpdiv0"
+			slot="info"
+			style="display: inline-block; font-size: 90%; padding-bottom: 7px; overflow: hidden; height: 245px;"
+		>
+			<p class="style1">
+				This setting controls how the Bible text is presented.
+			</p>
+			<p class="style1">
+				If “Verse Break” is selected, each verse will begin on a new line.
+			</p>
+			<p class="style1">
+				If “Paragraph” is selected, the Bible will be presented in paragraph
+				format, with one verse right after another, breaking only on new
+				paragraphs.
+			</p>
+			<p class="style1">
+				If “Reading” is selected, the Bible will be presented in prose format,
+				with one verse right after another, breaking only on new paragraphs. The
+				verse numbers will <span style="color:red;">not</span> be displayed.
+			</p>
+		</div>
+	</InfoBox>
+</table>
+
+<style>
+	p.style1 {
+		text-indent: 0;
+		text-align: left;
+		display: block;
+		font-size: 90%;
+	}
+</style>
---
+++ src/routes/preferences/InfoBox.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import moreInfo from '$lib/assets/moreinfo.png';
+	let showInfo = false;
+</script>
+
+<tr>
+	<!-- The main Table data -->
+	<td class="name">
+		<slot name="name"><!-- optional fallback --></slot>
+	</td>
+	<td class="options">
+		<slot name="options"><!-- optional fallback --></slot>
+	</td>
+	<td class="info-icon">
+		<input
+			type="image"
+			alt="moreinfo"
+			name="help"
+			src={moreInfo}
+			on:click={() => (showInfo = !showInfo)}
+		/>
+	</td>
+</tr>
+
+{#if showInfo}
+	<tr class="moreinfo">
+		<slot name="info"><!-- optional fallback --></slot>
+	</tr>
+{/if}
+
+<tr>
+	<td class="settd_spc" colspan="3" />
+</tr>
+
+<style>
+	tr {
+		font-size: 80%;
+	}
+
+	.name {
+		font-weight: bold;
+		vertical-align: top;
+		text-align: left;
+	}
+
+	.options {
+		width: 65%;
+		vertical-align: top;
+	}
+
+	.info-icon {
+		width: 5%;
+		vertical-align: top;
+	}
+
+	.settd_spc {
+		border-top: 1px dashed #aaaaaa;
+		height: 11px;
+	}
+</style>
